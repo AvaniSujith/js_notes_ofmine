a80eea27e0f97c615039ac983725aef6bc7b6931
AvaniSujith <chinchuavani123@gmail.com>
2024-11-05 15:33:27 +0530

    new notes

---
+++ freecodecamp/javascript.js
@@ -0,0 +1,173 @@
+// Always use camelCase 
+var myName = 'Avani';
+console.log(myName);
+
+
+//number
+
+var a;
+var b;
+a=7;
+b=5;
+console.log('a=' + a + ' ' + 'b=' + b);
+
+b=a;
+console.log('a=' + a + ' ' + 'b=' + b);
+
+
+//Maths
+
+var firstNumber = 20;
+var secondNumber = 10;
+
+var sumOfNumbers = firstNumber + secondNumber;
+console.log(sumOfNumbers);
+
+
+var differenceOfNumber = firstNumber - secondNumber;
+console.log(differenceOfNumber);
+
+
+//increment decrement etc
+
+var myVar = 87;
+myVar ++;
+console.log(myVar);
+
+myVar --;
+console.log(myVar);
+
+var remainder = 11%4;
+console.log(remainder);
+
+var x = 10;
+var y = 20;
+var z = 19;
+
+x += 10;
+y += 20;
+z += 22;
+console.log(x, y, z);
+
+
+
+x -= 2;
+y -= 1;
+z -= 3;
+console.log(x, y, z);
+
+
+x *= 2;
+y *= 3;
+z *= 4;
+console.log(x, y, z);
+
+
+x /= 2;
+y /=3;
+z /=4;
+console.log(x, y, z);
+
+//literals problems escape
+var myStr = "My name is \"Avani\" helloo \"haiii\" how are you \"doing?\"";
+console.log(myStr);
+
+// const num = 20;
+var num =20;
+num ++;
+console.log(num);
+
+
+
+
+var firstString ='Im happy';
+firstString += 'im okay';
+console.log(firstString)
+
+var mySelf = 'Avani';
+var fullSentnece = 'My name is ' + mySelf;
+console.log(mySelf);
+console.log(fullSentnece);
+
+
+//Apending variables
+var awesome = ' Im';
+console.log(awesome.length);
+var good = ' feeling good';
+good += awesome;
+awesome += good;
+console.log(good);
+console.log(awesome);
+
+//length
+
+var count = good.length;
+console.log(count);
+
+//getting specific letters in position
+
+var myString = 'hello world';
+console.log(myString[0]);
+
+var notKnownLength = 'rithika';
+console.log(notKnownLength[notKnownLength.length-1]);
+console.log(notKnownLength[notKnownLength.length-2]);
+console.log(notKnownLength[notKnownLength.length-3]);
+console.log(notKnownLength[notKnownLength.length-4]);
+console.log(notKnownLength[notKnownLength.length-5]);
+console.log(notKnownLength[notKnownLength.length-6]);
+console.log(notKnownLength[notKnownLength.length-7]);
+console.log(notKnownLength[notKnownLength.length-8]);
+
+//sententce creation
+
+function sententce(name, title, feeling){
+    var result = 'My name is '+ name +'. Im a ' + title + '. Im feeling '+ feeling;
+    return result;
+
+}
+
+console.log(sententce("avani", "student", "good"));
+console.log(sententce('lia','doctor','excellent'));
+
+
+//arrays
+
+var ourArray = ['John', 22];
+console.log(ourArray);
+
+var myArray = [1,22,99];
+myArray[1]= 0;
+console.log(myArray);
+
+var nestesdArray = [['one','two'],['two','three']];
+console.log(nestesdArray[0][1]);
+console.log(nestesdArray[0,1]);
+
+
+var names = ['hari', 'mahi', 'deepth', 'lia', 'lency'];
+names.push(['gayu', 'ram', 'sam']);
+console.log(names);
+
+myArray.pop();
+// myArray.shift();
+
+console.log(myArray);
+
+
+myArray.unshift('avani');
+console.log(myArray);
+
+
+//shopping list
+var myList =[['apple',3],['soap',2],['banana',6],['icecream',9]];
+console.log(myList[0][0]);
+
+
+
+//function
+function haiWorld(){
+    console.log('hello world');
+};
+haiWorld();
+haiWorld();
