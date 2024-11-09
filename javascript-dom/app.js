7f689405685856f7d070570f410ad36589739ad2
AvaniSujith <chinchuavani123@gmail.com>
2024-11-09 21:14:19 +0530

    new note

---
+++ javascript-dom/app.js
@@ -0,0 +1,29 @@
+//Get element by Id
+
+const heading = document.getElementById('main-heading');
+console.log(heading);
+
+
+//classname
+
+const list = document.getElementsByClassName('list-items');
+console.log(list);
+
+
+//tagname
+
+const listItem = document.getElementsByTagName('li');
+console.log(listItem);
+
+
+//queryselector
+
+const container = document.querySelector('div');
+console.log(container);
+
+//query selectorall
+
+const containers = document.querySelectorAll('div');
+console.log(containers);
+
+
