c691631fa905a5be3a211a507ae5320c5573b6e1
AvaniSujith <chinchuavani123@gmail.com>
2024-11-14 10:21:26 +0530

    new note - js

--- practice_qnt/js_10.js
+++ practice_qnt/js_10.js
@@ -97,20 +97,39 @@
 
 //version-10.6
 
+// function primeORNot(num){
+//     for(i=2; i< num; i++){
+//      if( num % i === 0){
+//          console.log("The given number is not a prime number");
+//          return;
+//     }
+//     else{
+//         console.log("The given number is a prime number");
+//         break;
+//     }
+//      }
+     
+    
+// }
+//  primeORNot(19);
+//  primeORNot(15);
+//  primeORNot(30);
+
+// version-10.7
+
 function primeORNot(num){
-    for(i=2; i< num;i++){
+    for(i=2; i< num; i++){
      if( num % i === 0){
          console.log("The given number is not a prime number");
-         break;
+         return;
     }
-    else{
-        console.log("The given number is a prime number");
+  
     }
+    console.log("The given number is a prime number");
      }
      
     
-}
-//  primeORNot(19);
- primeORNot(15);
- primeORNot(30);
 
+ primeORNot(76);
+//  primeORNot(15);
+//  primeORNot(30);
