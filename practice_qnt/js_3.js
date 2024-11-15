6d2c802cd6563302454d81c60d6b4f791d874639
AvaniSujith <chinchuavani123@gmail.com>
2024-11-15 12:52:44 +0530

    Question 3- testcase_ final

--- practice_qnt/js_3.js
+++ practice_qnt/js_3.js
@@ -1,13 +1,66 @@
 // version-3.1
 
+// function checkEligibility(age){
+//     if( age >= 18){
+//         console.log("Eligible for voting");
+//     }
+//     else{
+//         console.log("Not eligible for voting");
+//     }
+// }
+
+// checkEligibility(2);
+// checkEligibility(33);
+
+
+// version - 3.2 
+
 function checkEligibility(age){
     if( age >= 18){
-        console.log("Eligible for voting");
+        return "Eligible for voting";
     }
     else{
-        console.log("Not eligible for voting");
+        return "Not eligible for voting";
     }
 }
 
-checkEligibility(2);
-checkEligibility(33);
+function testForEligibility(){
+    const testCases = [
+        {
+            input : 22,
+            output : "Eligible for voting"
+        },
+        {
+            input : 33,
+            output : "Eligible for voting"
+        },
+        {
+            input : 77,
+            output : "Eligible for voting"
+        },
+        {
+            input : 3,
+            output : "Not eligible for voting"
+        },
+        {
+            input : 5,
+            output : "Not eligible for voting"
+        },
+        {
+            input : 9,
+            output : "Eligible for voting"
+        }
+    ]
+
+    testCases.forEach((testCase , index) =>{
+        const result = checkEligibility(testCase.input);
+
+        if(result == testCase.output){
+            console.log(`Test case ${index + 1} passed`);
+        }else{
+            console.log(`Test case ${index + 1} failed\nCorrect output - ${result}`);
+        }
+    })
+}
+
+testForEligibility();
