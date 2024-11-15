29fcdcbde6c4e866800949a4a5856e93a58a05b6
AvaniSujith <chinchuavani123@gmail.com>
2024-11-15 14:16:48 +0530

    Question 9- testcase_ final

--- practice_qnt/js_9.js
+++ practice_qnt/js_9.js
@@ -56,6 +56,41 @@
 
 // version-9.4
 
+// function averageValue(arr){
+//     let i =0;
+//     let sum = 0;
+//     for(i=0; i<arr.length;i++){
+      
+//         sum += arr[i];
+//        avg = sum/arr.length;
+// }
+     
+//     console.log(avg);
+   
+// }
+
+// let array = [1.5, 2.5, 3.5];
+// averageValue(array);
+
+// let array1 = [1000000, 5000000, 3000000, 7000000, 9000000];
+// averageValue(array1);
+
+// let array2 = [2, 4, 6, 8, 10];
+// averageValue(array2);
+
+// let array3 = [3, -7, 5, -2, 9];
+// averageValue(array3);
+
+// let array4 = [0, 0, 0, 0];
+// averageValue(array4);
+
+// let empty = [];
+// averageValue(empty);
+
+// version - 9.5 using test cases 
+
+
+
 function averageValue(arr){
     let i =0;
     let sum = 0;
@@ -65,25 +100,39 @@ function averageValue(arr){
        avg = sum/arr.length;
 }
      
-    console.log(avg);
+    return avg;
    
 }
 
-let array = [1.5, 2.5, 3.5];
-averageValue(array);
+function testAverageValue(){
+    const testCases = [
+        {
+            input : [2, 4, 6, 8, 10],
+            output : 6
+        },
+        {
+            input : [-5, 10, 15, -20, 25],
+            output : 5
+        },
+        {
+            input : [1.5, 2.5, 3.5],
+            output : 2.5
+        },
+        {
+            input : [0,0,0,0],
+            output : 4
+        }
+    ]
 
-let array1 = [1000000, 5000000, 3000000, 7000000, 9000000];
-averageValue(array1);
+    testCases.forEach((testCase, index) => {
+        const result = averageValue(testCase.input);
+        
+        if(result == testCase.output){
+            console.log(`Test case ${index+1} passed`);
+        }else{
+            console.log(`Test case ${index +1} failed \nCorrect output - ${result}`)
+        }
+    });
+}
 
-let array2 = [2, 4, 6, 8, 10];
-averageValue(array2);
-
-let array3 = [3, -7, 5, -2, 9];
-averageValue(array3);
-
-let array4 = [0, 0, 0, 0];
-averageValue(array4);
-
-let empty = [];
-averageValue(empty)
-
+testAverageValue();
