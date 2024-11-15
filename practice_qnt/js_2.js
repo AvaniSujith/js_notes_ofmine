d00e393e9b8bfac9c4dff7c3fa072f6d71bbb8ca
AvaniSujith <chinchuavani123@gmail.com>
2024-11-15 12:03:20 +0530

    Question 2- testcase_ final

--- practice_qnt/js_2.js
+++ practice_qnt/js_2.js
@@ -18,33 +18,98 @@
 // version-2.2
 
 
+// function checkForVowel(letter){
+ 
+//     if(letter == "a" || letter == "A"){
+//         console.log("The given alphabet is vowel");
+        
+//     }
+//     else  if(letter == "e" || letter == "E"){
+//         console.log("The given alphabet is vowel");
+        
+//     }
+//     else  if(letter == "i" || letter == "I"){
+//         console.log("The given alphabet is vowel");
+        
+//     }
+//     else  if(letter == "o" || letter == "O"){
+//         console.log("The given alphabet is vowel");
+        
+//     }
+//     else  if(letter == "u" || letter == "U"){
+//         console.log("The given alphabet is vowel");
+        
+//     }
+//     else{
+//         console.log("The given alphabet is consonent");
+//     }
+// }
+// checkForVowel("o");
+// checkForVowel('k');
+// checkForVowel('t');
+// checkForVowel("A");
+
+
+
+// version -2.3 using testcases 
+
 function checkForVowel(letter){
  
     if(letter == "a" || letter == "A"){
-        console.log("The given alphabet is vowel");
+        return "The given alphabet is vowel";
         
     }
     else  if(letter == "e" || letter == "E"){
-        console.log("The given alphabet is vowel");
+        return "The given alphabet is vowel";
         
     }
     else  if(letter == "i" || letter == "I"){
-        console.log("The given alphabet is vowel");
+        return "The given alphabet is vowel";
         
     }
     else  if(letter == "o" || letter == "O"){
-        console.log("The given alphabet is vowel");
+        return "The given alphabet is vowel";
         
     }
     else  if(letter == "u" || letter == "U"){
-        console.log("The given alphabet is vowel");
+        return "The given alphabet is vowel";
         
     }
     else{
-        console.log("The given alphabet is consonent");
+        return "The given alphabet is consonent";
     }
 }
-checkForVowel("o");
-checkForVowel('k');
-checkForVowel('t');
-checkForVowel("A");
+
+function testForVowels(){
+    const testCases = [
+        {
+            input : 'U',
+            output : "The given alphabet is vowel"
+        },
+        {
+            input : 'i',
+            output : 'The given alphabet is vowel'
+        },
+        {
+            input : 'k',
+            output : 'The given alphabet is consonent'
+        },
+        {
+            input : 'g',
+            output : "The given alphabet is consonent"
+        }
+    ]
+
+
+        testCases.forEach((testCase, index) =>{
+            const result = checkForVowel(testCase.input);
+
+            if(result == testCase.output){
+                console.log(`Test case ${index + 1} passed`);
+            }else{
+                console.log(`Test case ${index + 1} failed`);
+            }
+        });
+}
+
+testForVowels();
