29d6d14f6bd874e394c42e6f44ec536249d933b5
AvaniSujith <chinchuavani123@gmail.com>
2024-11-15 13:15:58 +0530

    Question 5- testcase_ final

--- practice_qnt/js_5.js
+++ practice_qnt/js_5.js
@@ -30,10 +30,50 @@
 
 // version-5.4
 
-function SumOfNatural(x){
+// function SumOfNatural(x){
         
-    return x*(x+1)/2;
+//     return x*(x+1)/2;
     
-}
+// }
 
-console.log(SumOfNatural(8));
+// console.log(SumOfNatural(8));
+
+// version - 5.5 using testcases 
+
+ function SumOfNatural(x){
+        
+        return x*(x+1)/2;
+        
+    }
+
+function testSum(){
+    const testCases = [
+        {
+            input : 5,
+            output : 15
+        },
+        {
+            input : 10,
+            output : 55
+        },
+        {
+            input : 3,
+            output : 6
+        }, 
+        {
+            input : 100,
+            output : 5000
+        }
+    ]
+
+    testCases.forEach((testCase, index) =>{
+        const result = SumOfNatural(testCase.input);
+
+        if(result == testCase.output){
+            console.log(`Test case ${index + 1} passed`);
+        }else{
+            console.log(`Test case ${index + 1} failed\nCorrect output - ${result}`);
+        }
+    })
+}
+testSum();
