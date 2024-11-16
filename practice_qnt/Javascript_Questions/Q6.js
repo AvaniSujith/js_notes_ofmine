71f66dc5ddbd65a0ba2612e989633b07fc2785cc
AvaniSujith <chinchuavani123@gmail.com>
2024-11-16 14:18:25 +0530

    js-6question

---
+++ practice_qnt/Javascript_Questions/Q6.js
@@ -0,0 +1,11 @@
+// Solve a linear equation in one variable( ax + b = c ).
+
+
+function linearEqn(a,b,c){
+    return x = (c - b) / a;
+}
+
+console.log(linearEqn(3,4,10));
+console.log(linearEqn(2,5,11));
+console.log(linearEqn(-3,7,1));
+console.log(linearEqn(4,-8,0));
