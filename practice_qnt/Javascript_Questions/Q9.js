a82615c61b3dfc6bbdec5398c9867db35ccc9e6c
AvaniSujith <chinchuavani123@gmail.com>
2024-11-16 15:07:48 +0530

    js-9question

---
+++ practice_qnt/Javascript_Questions/Q9.js
@@ -0,0 +1,10 @@
+//perimeter of a triangle
+
+function trianglePeri(a,b,c){
+    let perimeter = a + b+ c; 
+    console.log(perimeter);
+}
+
+trianglePeri(5,7,10);
+trianglePeri(3,4,5);
+trianglePeri(6,8,10);
