350d7c6863cdc457f4cd26ba754a760974670cbc
AvaniSujith <126156788+AvaniSujith@users.noreply.github.com>
2025-01-14 07:37:16 +0530

    JS projects

--- DOM_Projects/Project_38/app.js
+++ DOM_Projects/Project_38/app.js
@@ -2,7 +2,7 @@ const contents = document.querySelectorAll('.content')
 const listItems = document.querySelectorAll('nav ul li')
 
 listItems.forEach((item, idx) => {
-    item.addEventListener('click', ()=>{
+    item.addEventListener('click', () => {
         hideAllContents()
         hideAllItems()
 
@@ -11,11 +11,11 @@ listItems.forEach((item, idx) => {
     })
 })
 
-
-function hideAllContents(){
+function hideAllContents() {
     contents.forEach(content => content.classList.remove('show'))
 }
 
-function hideAllItems(){
+
+function hideAllItems() {
     listItems.forEach(item => item.classList.remove('active'))
 }
