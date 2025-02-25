a2b2b8fc04f436478879512ab6f1f2bfd3477b82
AvaniSujith <126156788+AvaniSujith@users.noreply.github.com>
2025-02-25 23:43:40 +0530

    new file

--- learning_workout/javascript_practicing/interview_practice/Qtn.js
+++ learning_workout/javascript_practicing/interview_practice/Qtn.js
@@ -1,27 +1,100 @@
-// Q1. Trapping Rainwater Problem states that given an array of n non-negative integers arr[] representing an elevation map where the width of each bar is 1, compute how much water it can trap after rain.
+// Q1. second largest element in an array 
 
-// Trapping Rainwater Problem
-// Trapping Rainwater Problem
+// Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
 
-// Examples:  
+// Note: If the second largest element does not exist, return -1.
 
-// Input: arr[] = [3, 0, 1, 0, 4, 0, 2]  - min(3,4) - 0 = 3, min(3, 4) - 1 = 2, min(3, 4) -  0 = 3, min(3, 2) - 4 = -2, min(4, 2) - 0 = 2// Output: 10
-// Explanation: The expected rainwater to be trapped is shown in the above image.
+// Examples:
+
+// Input: arr[] = [12, 35, 1, 10, 34, 1]
+// Output: 34
+// Explanation: The largest element of the array is 35 and the second largest element is 34.
 
 
-// Input: arr[] = [3, 0, 2, 0, 4]
-// Output: 7
-// Explanation: We trap 0 + 3 + 1 + 3 + 0 = 7 units.
+// Input: arr[] = [10, 5, 10]
+// Output: 5
+// Explanation: The largest element of the array is 10 and the second largest element is 5.
 
-function waterCollection(height){
-    let left = [];
-    let height = [];
-    let right = [];
-    let water = 0; 
 
-    for(let i = 0; i< arr.length - 1; i++){
-        left[0] = height[0];
-        for(let j = 1; j < arr.length -1; j++){
-            left[i] = min()
+// Input: arr[] = [10, 10, 10]
+// Output: -1
+// Explanation: The largest element of the array is 10 there is no second largest element.
+
+// function secondLargest(arr){
+//     // let sortArr = sort(arr);
+//     let sortArr = arr.sort((a,b) => b - a)
+//     // let sortArr = arr.sort((a, b)=> a - b)
+//     return sortArr[1];
+// }
+
+// let arr = [10, 5, 10]
+
+// console.log(secondLargest(arr));
+
+
+// function secondLargest(arr){
+//     let sortArr = [];
+//     let maxValue = arr[0]
+//     for(let i = 0; i < arr.length; i++){
+//         if(maxValue < arr[i] && !sortArr.includes(arr[i])){
+//             sortArr.push(arr[i]);
+//         }
+//     }
+
+//     return sortArr;
+// }
+
+
+// let arr = [10, 5, 10];
+// console.log(secondLargest(arr))
+
+function secondLargest(arr){
+    // let sortArr = [];
+    // let maxValue = arr[0];
+
+    // for(let i = 0; i<arr.length; i++){
+    //     if(maxValue < arr[i]){
+    //         maxValue = arr[i];
+    //     }
+    // }
+
+    // for(let i = 0; i < arr.length; i++){
+    //     if(!sortArr.includes(arr[i])){
+    //         sortArr.push(arr[i])
+    //     }
+    // }
+
+    // for(let i = 0; i < sortArr.length; i++){
+    //     for(let j = i+ 1; j<sortArr.length; j++){
+    //         if(sortArr[i]<sortArr[j]){
+    //             let temp = sortArr[i];
+    //             sortArr[i] = sortArr[j];
+    //             sortArr[j] = temp;
+    //         }
+    //     }
+    // }
+    // return sortArr                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           z
+
+    let sortArr = [];
+    let newArr = [];
+
+    sortArr = arr.sort((a, b) => b - a);
+
+    for(let i = 0; i < arr.length; i++){
+        let val = arr[i]
+        for(let j = 0; j < sortArr.length; j++){
+            if(val !== sortArr[j]){
+                newArr.push(val);
+            }
         }
+    }
+
+    // return sortArr[1]
+
+    return newArr[1];
+    
 }
+
+// let arr  = [12, 35, 1, 10, 34, 1];
+let arr = [10, 5, 10]
+console.log(secondLargest(arr));
