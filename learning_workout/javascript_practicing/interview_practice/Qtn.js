653a3c5965401ada2aa0c16cc79670290ce74ce8
AvaniSujith <chinchuavani123@gmail.com>
2025-08-09 23:57:42 +0530

    new qtn

--- learning_workout/javascript_practicing/interview_practice/Qtn.js
+++ learning_workout/javascript_practicing/interview_practice/Qtn.js
@@ -693,8 +693,6 @@ function evenNumbers(numbers) {
 }
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
 const evenNumbersArray = evenNumbers(numbers);
-console.log("Even numbers:", evenNumbersArray);
-
 
 function sortArrayDesc(arr) {
     let n = arr.length;
